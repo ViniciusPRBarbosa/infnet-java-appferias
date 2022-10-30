2cc0630d517790287add8880579d2dcf3bea23e6
ViniciusPRBarbosa <41838174+ViniciusPRBarbosa@users.noreply.github.com>
2022-10-30 14:33:47 -0300

    Feature 02 - Relacionamento entre classes - Criando relacionamento entre classes e incluindo novo construtor

--- appferias/src/main/java/br/edu/infnet/appferias/PlanejamentoFeriasTeste.java
+++ appferias/src/main/java/br/edu/infnet/appferias/PlanejamentoFeriasTeste.java
@@ -9,6 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import br.edu.infnet.appferias.model.domain.PlanejamentoFerias;
+import br.edu.infnet.appferias.model.domain.Turista;
 
 @Order(4)
 @Component
@@ -20,16 +21,31 @@ public class PlanejamentoFeriasTeste implements ApplicationRunner {
 		System.out.println("======== TESTANDO IMPRESSÃO DE PLANEJAMENTOS DE FÉRIAS ========");
 
 		PlanejamentoFerias p1 = new PlanejamentoFerias();
+		
+		Turista t1 = new Turista();
+
+		t1.setNome("Vinícius Pereira Reis Barbosa");
+		t1.setEmail("vinicius.barbosa@emailteste.com");
+		t1.setPossuiPassaporte(true);
+		t1.setDataNascimento(LocalDateTime.of(1990, Month.APRIL, 30, 0, 0, 0));
 
 		p1.setTitulo("Primeira viagem para a Europa");
 		p1.setDescricao("Viagem para a Europa, com visita a parentes e a locais turisticos.");
 		p1.setDataInicio(LocalDateTime.of(2022, Month.MAY, 1, 8, 0, 0));
 		p1.setDataFim(LocalDateTime.of(2022, Month.MAY, 30, 23, 59, 0));
+		p1.setTurista(t1);
 		
 		System.out.println(p1.toString());
 		
-		PlanejamentoFerias p2 = new PlanejamentoFerias();
+		Turista t2 = new Turista();
 
+		t2.setNome("Bernard Pereira dos Reis Barbosa");
+		t2.setEmail("bernard.barbosa@emailteste.com");
+		t2.setPossuiPassaporte(false);
+		t2.setDataNascimento(LocalDateTime.of(1996, Month.DECEMBER, 23, 0, 0, 0));
+		
+		PlanejamentoFerias p2 = new PlanejamentoFerias(t2);
+		
 		p2.setTitulo("Primeira viagem para a Asia");
 		p2.setDescricao("Viagem para ao Japão, com visita a locais turisticos.");
 		p2.setDataInicio(LocalDateTime.of(2022, Month.AUGUST, 1, 8, 0, 0));
@@ -39,10 +55,16 @@ public class PlanejamentoFeriasTeste implements ApplicationRunner {
 		
 		PlanejamentoFerias p3 = new PlanejamentoFerias();
 
-		p3.setTitulo("Viagem a cidade de parentes");
+		Turista t3 = new Turista();
+
+		t3.setNome("Marina Garcia de Vechi");
+		t3.setEmail("marina.vechi@emailteste.com");
+		t3.setPossuiPassaporte(true);
+		t3.setDataNascimento(LocalDateTime.of(1991, Month.MARCH, 1, 0, 0, 0));
+		
 		p3.setDescricao("Viagem para cidade natal, para visita a parentes e a locais de infância.");
-		p3.setDataInicio(LocalDateTime.of(2022, Month.FEBRUARY, 1, 8, 0, 0));
-		p3.setDataFim(LocalDateTime.of(2022, Month.FEBRUARY, 27, 23, 59, 0));
+		p3.setDataFim(LocalDateTime.of(2022, Month.DECEMBER, 27, 23, 59, 0));
+		p3.setTurista(t3);
 		
 		System.out.println(p3.toString());
 		
--- appferias/src/main/java/br/edu/infnet/appferias/model/domain/PlanejamentoFerias.java
+++ appferias/src/main/java/br/edu/infnet/appferias/model/domain/PlanejamentoFerias.java
@@ -9,7 +9,25 @@ public class PlanejamentoFerias {
 	private LocalDateTime dataInicio;
 	private LocalDateTime dataFim;
 	private String descricao;
+	private Turista turista;
 	
+	public PlanejamentoFerias() {
+		titulo = "Novo planejamento de férias";
+		dataInicio = LocalDateTime.now();
+	}
+	
+	public PlanejamentoFerias(Turista turista) {
+		this.turista = turista;
+	}
+	
+	public Turista getTurista() {
+		return turista;
+	}
+
+	public void setTurista(Turista turista) {
+		this.turista = turista;
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
@@ -49,6 +67,12 @@ public class PlanejamentoFerias {
 		
 		StringBuilder impressaoObjeto = new StringBuilder();
 		
+		impressaoObjeto.append("Informações do turista:");
+		impressaoObjeto.append("\n");
+		impressaoObjeto.append(turista);
+		impressaoObjeto.append("\n");
+		impressaoObjeto.append("Informações do planejamento:");
+		impressaoObjeto.append("\n");
 		impressaoObjeto.append(String.format("Titulo: %s", titulo));
 		impressaoObjeto.append("\n");
 		impressaoObjeto.append(String.format("Descricao: %s", descricao));
