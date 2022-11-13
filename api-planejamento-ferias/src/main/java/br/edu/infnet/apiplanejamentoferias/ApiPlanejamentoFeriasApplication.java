0c2dc2eb6c814727d7d9f8e264032ef3f8161613
ViniciusPRBarbosa <41838174+ViniciusPRBarbosa@users.noreply.github.com>
2022-11-13 16:13:06 -0300

    Feature 4 - Configurações inciais das APIs

---
+++ api-planejamento-ferias/src/main/java/br/edu/infnet/apiplanejamentoferias/ApiPlanejamentoFeriasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apiplanejamentoferias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlanejamentoFeriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlanejamentoFeriasApplication.class, args);
+	}
+
+}
---
+++ api-turista/src/main/java/br/edu/infnet/apiturista/ApiTuristaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apiturista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTuristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTuristaApplication.class, args);
+	}
+
+}
